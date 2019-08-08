4d7864fdbe2d1d269066d9b8ec842a2be9596aea
cmokj <root@chenminjiadeMacBook-Pro.local>
2019-08-09 06:50:26 +0800

    将 Todo 列表变成 TodoItem 组件

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TodoInput from './TodoInput';
+import TodoItem from './TodoItem';
 import './App.css';
 
 class App extends Component {
@@ -7,16 +8,24 @@ class App extends Component {
     super();
     this.state = {
       newTodo: 'test',
-      todoList: [{
-        id: 1,
-        title: '第一个待办'
-      }]
-    }
-  }
+      todoList: [
+        {
+          id: 1,
+          title: '第一个待办'
+        },
+        {
+          id: 2,
+          title: '第二个待办'
+        }
+      ]
+    };
+  };
 
   render() {
     let todos = this.state.todoList.map((item, index) => {
-      return <li>{item.title}</li>;
+      return (
+        <li><TodoItem todo={item} /></li>
+      );
     })
     return (
       <div className="App">
