0ca8ab47055eac1a30d472cd2f82b929278a35b0
cmokj <root@chenminjiadeMacBook-Pro.local>
2019-08-10 10:32:39 +0800

    添加 UserDialog.css

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './UserDialog.css'
 
 export default class UserDialog extends Component {
     render() {
@@ -9,7 +10,7 @@ export default class UserDialog extends Component {
                         points="69.4453125 8.90625 17.5546875 60.796875 34.8515625 78.09375 69.4453125 43.5 86.7421875 26.203125"
                         class="x-hidden-focus"></polygon>
                     <div className="title">
-                        <span>To-Do</span>
+                        <h1>Microsoft To-Do</h1>
                     </div>
                     <div className="welcome">
                         <span>欢迎回来</span>
