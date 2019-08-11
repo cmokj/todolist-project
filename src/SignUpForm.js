25cac0971732a6b12dc1f5ee50aec0f0187dd244
cmokj <root@chenminjiadeMacBook-Pro.local>
2019-08-11 09:15:48 +0800

    将纯组件 SignUpForm 改为函数

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -1,59 +1,53 @@
 import React, { Component } from 'react';
 import { signUp } from './LeanCloud'
 
-export default class SignUpForm extends Component {
-    click() {
-        this.props.onChange.call();
+function onSignUp(props, e) {
+    e.preventDefault();
+    let { username, password, email } = props.formData;
+    let success = (user) => {
+        // this.props.onSignUp.call(null, user.attributes);
     }
-    signUp(e) {
-        e.preventDefault();
-        let { username, password, email } = this.props.formData;
-        let success = (user) => {
-            // this.props.onSignUp.call(null, user.attributes);
+    let error = (error) => {
+        switch (error.code) {
+            case 202:
+                alert('用户名已被占用！');
+                break;
+            default:
+                alert(error);
+                break;
         }
-        let error = (error) => {
-            switch (error.code) {
-                case 202:
-                    alert('用户名已被占用！');
-                    break;
-                default:
-                    alert(error);
-                    break;
-            }
-        }
-        signUp(email, username, password, success, error);
     }
-    render() {
-        return (
-            <div>
-                <form className="signUp">
-                    <div className="row">
-                        <input type="text"
-                            placeholder="someone@example.com"
-                            value={this.props.formData.email}
-                            onChange={this.props.changeFormData.bind(null, 'email')}
-                        />
-                    </div>
-                    <div className="row">
-                        <input type="text"
-                            placeholder="用户名"
-                            value={this.props.formData.username}
-                            onChange={this.props.changeFormData.bind(null, 'username')}
-                        />
-                    </div>
-                    <div className="row">
-                        <input type="password"
-                            placeholder="区分大小写"
-                            value={this.props.formData.password}
-                            onChange={this.props.changeFormData.bind(null, 'password')}
-                        />
-                    </div>
-                    <button type="submit"
-                        onClick={this.signUp.bind(this)}>注册</button>
-                    <a href="#" onClick={this.props.returnToSignIn.bind(this)}>返回登录</a>
-                </form>
-            </div>
-        )
-
-    }
+    signUp(email, username, password, success, error);
+}
+export default function (props) {
+    return (
+        <div>
+            <form className="signUp">
+                <div className="row">
+                    <input type="text"
+                        placeholder="someone@example.com"
+                        value={props.formData.email}
+                        onChange={props.changeFormData.bind(null, 'email')}
+                    />
+                </div>
+                <div className="row">
+                    <input type="text"
+                        placeholder="用户名"
+                        value={props.formData.username}
+                        onChange={props.changeFormData.bind(null, 'username')}
+                    />
+                </div>
+                <div className="row">
+                    <input type="password"
+                        placeholder="区分大小写"
+                        value={props.formData.password}
+                        onChange={props.changeFormData.bind(null, 'password')}
+                    />
+                </div>
+                <button type="submit"
+                    onClick={onSignUp.bind(null, props)}>注册</button>
+                <a href="#" onClick={props.returnToSignIn.bind(this)}>返回登录</a>
+            </form>
+        </div>
+    )
 }
